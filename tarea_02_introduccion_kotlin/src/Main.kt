6634e6979078f83dc6fff98b6a3ac2646b56c920
@patopollos <msg617521@lasallebajio.edu.mx>
2024-09-16 23:48:29 -0600

    tarea 2 introduccion a kotlin
    tarea 2 introduccion a kotlin

--- tarea_02_introduccion_kotlin/src/Main.kt
+++ tarea_02_introduccion_kotlin/src/Main.kt
@@ -11,24 +11,54 @@ fun main() {
             "Yogurt" to 150
         )
         //Mostrar la lista de alimentos y sus calorías.
-        println("Lista de alimentos y calorias: \n $alimentos")
-
+        println("Lista de alimentos y calorias: $alimentos")
+        println()
         /*
         * Separar los alimentos en dos categorías:
             alimentos con más de 500 calorías ("altos en calorías")
             y alimentos con menos de 500 calorías ("bajos en calorías").*/
         val altascalorias = alimentos.filter {it.value > 500}
         val bajascalorias = alimentos.filter {it.value < 500}
-        println("Alimentos altos en calorias: $altascalorias")
-        println("Alimentos bajos en calorias: $bajascalorias")
+        println("Alimentos altos en calorias (metodo filter): $altascalorias")
+        println("Alimentos bajos en calorias (metodo filter): $bajascalorias")
+        println()
 
+    /*
+        * Separar los alimentos en dos categorías por bucle for:
+            alimentos con más de 500 calorías ("altos en calorías")
+            y alimentos con menos de 500 calorías ("bajos en calorías").*/
+        var altos = 0
+        for ((key, value) in alimentos) {
+
+            if (value > 500){
+                println("Alimentos altos en calorias (metodo for) $key = $value")
+                altos++
+            }
+        }
+
+         for ((key, value) in alimentos) {
+            if (value < 500){
+            println("Alimentos bajos en calorias (metodo for) $key = $value")
+            }
+        }
+        println()
         //Calcular y mostrar el total de calorías consumidas.
         val totalcalorias = alimentos.values.sum()
-        println("Total de calorias consumidas: $totalcalorias calorias")
+        println("Total de calorias consumidas sum directo: $totalcalorias calorias")
+        println()
+        println("Total de calorias consumidas (sum a 2 variables): ")
+        println((altascalorias.values.sum()) + (bajascalorias.values.sum()))
+        println()
+        println("Total de calorias consumidas (sumando elemento por elemento): ")
+        println(alimentos.getValue(key = "Pizza") + alimentos.getValue(key = "Ensalada") + alimentos.getValue(key = "Hamburguesa") + alimentos.getValue(key = "Manzana") + alimentos.getValue(key = "Helado") + alimentos.getValue(key = "Pasta") + alimentos.getValue(key = "Yogurt"))
+        println()
 
         // Mostrar cuántos alimentos están en la categoría de "altos en calorías".
         val numeroaltascalorias = altascalorias.count()
-        println("Alimentos altos en calorias: $numeroaltascalorias")
+        println("Alimentos altos en calorias (metodo count): $numeroaltascalorias")
+        println()
+
+        println("Alimentos altos en calorias (imprimiendo variable): $altos ")
 
 
 }
